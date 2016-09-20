93e067e6b32b8d0149f5923ac769d91d95c83a0e
keke <keke@kmp.asiapacific.hpqcorp.net>
2016-09-20 11:44:57 +0800

    support docker compose v2

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,14 @@
-FROM java:latest
+FROM openjdk
 
 MAINTAINER Eric Lubow <eric@lubow.org>
+MAINTAINER keke <iamkeke@gmail.com>
 
 LABEL Description="Titan Graph DB with Gremlin to be used with Cassandra and Elasticsearch" Version="1.0" Tags="cassandra,elasticsearch,graph,gremlin,titan"
 
 WORKDIR /opt/titan-1.0.0-hadoop1
 
-RUN curl -o /opt/titan.zip http://s3.thinkaurelius.com/downloads/titan/titan-1.0.0-hadoop1.zip
-
+RUN curl -x $HTTP_PROXY -o /opt/titan.zip http://s3.thinkaurelius.com/downloads/titan/titan-1.0.0-hadoop1.zip
+RUN ls -al /opt
 RUN unzip /opt/titan.zip -d /opt/ && \
     rm /opt/titan.zip
 
--- run.sh
+++ run.sh
@@ -1,51 +1,7 @@
 #!/bin/bssh
 
 BIN=./bin
-SLEEP_INTERVAL_S=2
 
-# wait_for_startup friendly_name host port timeout_s
-wait_for_startup() {
-    local friendly_name="$1"
-    local host="$2"
-    local port="$3"
-    local timeout_s="$4"
-
-    local now_s=`date '+%s'`
-    local stop_s=$(( $now_s + $timeout_s ))
-    local status=
-
-    echo -n "Connecting to $friendly_name ($host:$port)"
-    while [ $now_s -le $stop_s ]; do
-        echo -n .
-        $BIN/checksocket.sh $host $port >/dev/null 2>&1
-        if [ $? -eq 0 ]; then
-            echo " OK (connected to $host:$port)."
-            return 0
-        fi
-        sleep $SLEEP_INTERVAL_S
-        now_s=`date '+%s'`
-    done
-
-    echo " timeout exceeded ($timeout_s seconds): could not connect to $host:$port" >&2
-    return 1
-}
-
-ELASTICSEARCH_STARTUP_TIMEOUT_S=60
-CASSANDRA_STARTUP_TIMEOUT_S=60
-
-wait_for_startup Elasticsearch \
-	$ELASTICSEARCH_PORT_9200_TCP_ADDR \
-	$ELASTICSEARCH_PORT_9200_TCP_PORT \
-	$ELASTICSEARCH_STARTUP_TIMEOUT_S || {
-   return 1
-}
-
-wait_for_startup Cassandra \
-	$CASSANDRA_PORT_9160_TCP_ADDR \
-	$CASSANDRA_PORT_9160_TCP_PORT \
-	$CASSANDRA_STARTUP_TIMEOUT_S || {
-	return 1
-}
 
 # use cassandra backed db instead of berkeleyje
 sed -i "s/host: localhost/host: 0.0.0.0/g" conf/gremlin-server/gremlin-server.yaml
@@ -55,6 +11,6 @@ sed -i "s/channelizer: org.apache.tinkerpop.gremlin.server.channel.WebSocketChan
 # create the backing file
 echo "gremlin.graph=com.thinkaurelius.titan.core.TitanFactory
 storage.backend=cassandra
-storage.hostname=$CASSANDRA_PORT_9160_TCP_ADDR" > conf/gremlin-server/titan-cassandra-server.properties
+storage.hostname=cassandra" > conf/gremlin-server/titan-cassandra-server.properties
 
 $BIN/gremlin-server.sh conf/gremlin-server/gremlin-server.yaml
